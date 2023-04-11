140199aee07b7a8962a077420a6f0dfc1b74cadb
LiuDongyang6 <392702533@qq.com>
2023-04-11 17:25:02 +0800

    clear unnecessary comments

--- classification/data_loader.py
+++ classification/data_loader.py
@@ -88,7 +88,7 @@ def get_IN_test_loader(data_dir,
     trans = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
-        transforms.ToTensor(),  # 将numpy数据类型转化为Tensor
+        transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ])
 
